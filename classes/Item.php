9ffca1dedf323a822ba73c753825cd8beffe0e94
j-pard <pardons.jonathan@gmail.com>
2020-04-02 16:46:11 +0200

    Item: left-section -> Class

--- classes/Item.php
+++ classes/Item.php
@@ -3,9 +3,35 @@
       class Item {
 
             public function create() {
-                  foreach ($this as $this->key => $this->value) {
-                        echo $this->key . " : " . $this->value . "<br>";
-                  }
+                  echo 
+                        "<section class='item-left'>
+                              <header>
+                                    <h3>" . ucfirst($this->title) . "</h3>
+                              </header>
+                              <div class='flex-row carousel'>
+                                    <div class='img-cont-left'>
+                                          <img src='" . $this->img . "'>
+                                    </div>
+                                    <div class='img-cont-right'>
+                                          <img src='" . $this->img2 . "'>
+                                          <img src='" . $this->img3 . "'>
+                                    </div>
+                              </div>
+                              <div class='flex-row item-more'>
+                                    <div class='likes-cont'><i class='fas fa-heart'></i>" . $this->likes . "</div>";
+                                    $form = new Form;
+                                          $form->create("controllers/liker.php", "POST");
+                                          $form->input("item-id", "number", $this->id, false);
+                                          $form->submit("Like");
+                                          $form->end();
+                        echo  "</div>
+                              <footer>
+                                    <div class='author-cont'><i class='fas fa-clock'></i>" . ucfirst($this->author) . "</div>
+                                    <div class='author-cont'><i class='fas fa-pen-fancy'></i>" . ucfirst($this->author) . "</div>
+                              </footer>
+                        </section>";
+
+                        //<section class='item-right'></section>
             }
       }
 ?>
--- views/item.php
+++ views/item.php
@@ -5,20 +5,46 @@
       Autoloader::register();
 
       $db = new Database($dbid_name, $dbid_user, $dbid_pass, $dbid_host);
-
-      if(isset($_GET['item-id'])) {
-            $id = $_GET['item-id'];
-
-            $sql = ('SELECT * FROM articles WHERE id = ?');
-            $values = array($id);
-
-            $item = $db->prepareItem($sql, $values, 'Item');
-            
-            $item->create();
-            
-
-      }
-      else {
-            echo "Wrong way";
-      }
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="../assets/css/style.css">
+      <meta name="author" content="Jonathan PARDONS">
+      <meta name="organization" content="BeCode">
+      <script src="https://kit.fontawesome.com/126bbe9047.js" crossorigin="anonymous"></script>
+      
+      <title>MTR - My Tech Ressources</title>
+</head>
+<body>
+      <header>
+      
+      </header>
+
+      <main>
+            <?php
+                  if(isset($_GET['item-id'])) {
+                        $id = $_GET['item-id'];
+            
+                        $sql = ('SELECT * FROM articles WHERE id = ?');
+                        $values = array($id);
+            
+                        $item = $db->prepareItem($sql, $values, 'Item');
+                        
+                        $item->create();
+                        
+            
+                  }
+                  else {
+                        echo "Wrong way";
+                  }
+
+            ?>
+      </main>
+      <script src="./assets/js/script.js"></script>
+</body>
+</html>
